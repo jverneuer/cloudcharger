2dd19f7d0bdedd3d942af39058685f6cf63120a9
Jens Verneuer <esmiralda.chunky@gmail.com>
2021-01-06 18:05:57 +0100

    cleaning up unused files

--- src/ledonoff.h
+++
@@ -1,9 +0,0 @@
-#include <queue.h>
-#include <Arduino_FreeRTOS.h>
-#include <queue.h>
-
-#define turnLedOffPriority ( tskIDLE_PRIORITY + 2 )
-#define turnLedOnPriority ( tskIDLE_PRIORITY + 1 )
-#define mainQUEUE_LENGTH 10
-
-
